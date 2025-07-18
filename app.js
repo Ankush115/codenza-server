29e1dbccdb65243ab56958f70731aec358a07d38
Ankush115 <alwagh.45@gmail.com>
2025-07-18 12:48:01 +0530

    Push latest

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var stdRouter = require("./routes/student");
+var cors = require("cors");
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -39,8 +41,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
-
-
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const mongodb = require("mongodb");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.post("/reg", async function (req, res, next) {
+  try {
+    const name = req.body.name;
+    const loc = req.body.loc;
+    const phone = req.body.phone;
+    const email = req.body.email;
+
+    const url =
+      "mongodb+srv://alwagh45:stepup@codenzaproject.6exl2o1.mongodb.net/";
+    const mongoClient = mongodb.MongoClient;
+
+    const server = await mongoClient.connect(url);
+
+    const db = server.db("stepup");
+
+    const collection = db.collection("users");
+
+    const result = await collection.insertOne({ name, loc, phone, email });
+
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.send(error.message);
+  }
 });
 
 module.exports = router;
